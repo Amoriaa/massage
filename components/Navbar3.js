f8bbead23c01922af4480258993621463832c70b
Aleksandra Fjodorova <busjka98@inbox.lv>
2020-10-15 21:10:06 +0300

    modified

--- components/Navbar3.js
+++ components/Navbar3.js
@@ -3,7 +3,7 @@ const Navbar3=()=>(
    
 
     <div className="menu">
-        <div className="navi"><Link href={'/index-eng'}><a className="nav">Main</a></Link>
+        <div className="navi"><Link href={'/index-eng'}><a className="nav">Home</a></Link>
             <div className="navi"><Link href={'/about-eng'}><a className="nav">Services</a></Link>
                 <div className="navi"><Link href={'/contacts-eng'}><a className="nav">Contacts</a></Link>
                 </div>
